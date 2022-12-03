0795cc8c3f61d6a7ebfa2fcf760e3308fd54f761
MikeWencel <wencel.michael@gmail.com>
2022-12-03 18:43:11 +0100

    ADD: Error handling, self server restart, new command to start server: npm start

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const animalsRoutes = require('./api/routes/animals');
 const peopleRoutes = require('./api/routes/people');
 
-app.use('/animals', animalsRoutes)
-app.use('/people', peopleRoutes)
+app.use(morgan('dev'))
+
+app.use('/animals', animalsRoutes);
+app.use('/people', peopleRoutes);
+
+app.use((req, res, next)=>{
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message:error.message
+        }
+    });
+});
 
 module.exports = app;
